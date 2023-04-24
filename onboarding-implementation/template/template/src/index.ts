7fc4b2e82f6de772efd6d8fcb28b040cd49b7c0a
kmilo8346 <kmilo8346@gmail.com>
2023-04-24 17:40:56 -0400

    feature(onboarding): fix bad template

--- onboarding-implementation/template/template/src/index.ts
+++
@@ -1,7 +0,0 @@
-import Module, { IModuleProps } from './bundle'
-
-export class TestSourceOverrideModule extends Module {
-    constructor(props: IModuleProps) {
-        super(props, {});
-    }
-}
