a27b4eee60b9fe73b1a70a3a807181c729b9cba9
kmilo8346 <kmilo8346@gmail.com>
2023-05-16 11:21:55 -0400

    feature(blank-implementation): added import of css source

--- .blank-implementation-template/template/src/index.ts
+++ .blank-implementation-template/template/src/index.ts
@@ -1,4 +1,5 @@
 import Module, { IModuleProps } from './bundle'
+import './bundle/index.css'
 
 export class {{#pascalCase data.name}}{{/pascalCase}}OverrideModule extends Module {
     constructor(props: IModuleProps) {
