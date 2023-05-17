7fc103155e423846d8d9c51c2f687febb284e435
Edwin Encinas <edwin.encinas.q@gmail.com>
2023-05-17 12:05:32 -0400

    feat: It was removed modules that were created for testing.

--- cencosudx-authentication-implementation/template/src/index.ts
+++
@@ -1,7 +0,0 @@
-import Module, { IModuleProps } from './bundle'
-
-export class {{#pascalCase data.name}}{{/pascalCase}}OverrideModule extends Module {
-    constructor(props: IModuleProps) {
-        super(props, {});
-    }
-}
