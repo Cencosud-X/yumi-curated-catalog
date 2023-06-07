94fe6db4043ef74cfac3025934851f0d2690fcfd
kmilo8346 <kmilo8346@gmail.com>
2023-06-07 10:44:44 -0400

    feature(price-news): remove the logic for css

--- price-news-implementation/template/src/index.ts
+++ price-news-implementation/template/src/index.ts
@@ -1,5 +1,4 @@
 import Module, { IModuleProps } from './bundle'
-import './bundle/index.css'
 
 export class {{#pascalCase data.name}}{{/pascalCase}}OverrideModule extends Module {
     constructor(props: IModuleProps) {
