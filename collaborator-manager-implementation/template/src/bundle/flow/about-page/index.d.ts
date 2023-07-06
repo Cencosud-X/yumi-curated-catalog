30da56e17e90b06b66c2b74066f590d900457b32
kmilo8346 <kmilo8346@gmail.com>
2023-07-06 16:15:09 -0400

    feature(catalog): remopve unused modules

--- collaborator-manager-implementation/template/src/bundle/flow/about-page/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Components } from '../../sdk';
-export interface IProps extends Components.IPageProps {
-}
-export interface IState {
-}
-export declare class AboutPage extends Components.Page<IProps, IState> {
-    state: IState;
-    render(): import("react/jsx-runtime").JSX.Element;
-}
--- identity-and-access-manager-implementation/template/src/index.ts
+++
@@ -1,8 +0,0 @@
-import Module, { IModuleProps } from './bundle'
-import './bundle/index.css'
-
-export class {{#pascalCase data.name}}{{/pascalCase}}OverrideModule extends Module {
-    constructor(props: IModuleProps) {
-        super(props, {});
-    }
-}
