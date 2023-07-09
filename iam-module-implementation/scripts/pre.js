cacf8771a1ec34a86e4c16c3e41295862cbe0f62
kmilo8346 <kmilo8346@gmail.com>
2023-07-08 23:26:08 -0400

    feature(iam-module): added staging url

--- iam-module-implementation/scripts/pre.js
+++ iam-module-implementation/scripts/pre.js
@@ -24,6 +24,7 @@ module.exports = async (runner, args) => {
         'npm install @awesome-cordova-plugins/in-app-browser@^6.3.0',
         'npm install @team_yumi/sdk@0.0.1-next.20230707-6e2cfb1-d6dab37be5f3fc0e3775695bfd8e6bfc',
         'npm install @team_yumi/ramen@0.0.3-next.1688753932673',
+        'npm install @capacitor/core@^4.8.0',
       ],
       {
         cwd: rc.workspace_path,
