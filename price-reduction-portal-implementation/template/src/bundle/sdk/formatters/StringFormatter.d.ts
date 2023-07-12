039f01fb0b689c9cb4cada62a4c70c6f6d904b0b
Ronald Espinoza <ing.ronaldespinoza@gmail.com>
2023-07-12 16:14:53 -0500

    feat(price-reduction-portal-implementation): Publishing new release

---
+++ price-reduction-portal-implementation/template/src/bundle/sdk/formatters/StringFormatter.d.ts
@@ -0,0 +1,16 @@
+export declare class StringFormatter {
+    /**
+     * Sanitize a string to be used as a name
+     * @param name
+     * @returns {string}
+     */
+    sanitize(name: string): string;
+    /**
+     * Convert string to snake case
+     * @param text
+     * @returns {string}
+     */
+    toSnakeCase(text: string): string;
+}
+declare const _default: StringFormatter;
+export default _default;
