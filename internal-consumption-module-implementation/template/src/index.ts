4708f1291bedb2f324b6435094795416d8d30262
Adrian Fernandez <adrianfdez469@gmail.com>
2023-07-17 10:46:17 -0400

    fix(internal-consuption-module): fix

--- internal-consumption-module-implementation/template/src/index.ts
+++ internal-consumption-module-implementation/template/src/index.ts
@@ -1,7 +1,7 @@
 import Module, { IModuleProps } from './bundle'
 import './bundle/index.css'
 
-export class {{#pascalCase data.name}}{{/pascalCase}}OverrideModule extends Module {
+export class InternalConsumptionOverrideModule extends Module {
     constructor(props: IModuleProps) {
         super(props, {});
     }
