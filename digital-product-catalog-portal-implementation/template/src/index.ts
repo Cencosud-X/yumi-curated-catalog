a0db2cb7fabe64825e8347824aa71fa48e7f91f7
ChinetePro <chinetepro@MacBook-Air-de-ChinetePro.local>
2023-08-04 14:46:34 -0300

    feat(digital-product-catalog-portal-source): Publishing new release

--- digital-product-catalog-portal-implementation/template/src/index.ts
+++ digital-product-catalog-portal-implementation/template/src/index.ts
@@ -1,7 +1,7 @@
-import Module, { IModuleProps, Footer, Header, CartContextProvider, GlobalContextProvider, ElevatorButton, LoaderScreen, ModalCart } from './bundle'
+import Module, { IModuleProps, Footer, Header, CartContextProvider, GlobalContextProvider, ElevatorButton, DrawerCategories, LoaderScreen, ModalCart } from './bundle'
 import './bundle/index.css'
 
-export { Footer, Header, CartContextProvider, GlobalContextProvider, ElevatorButton, LoaderScreen, ModalCart }
+export { Footer, Header, CartContextProvider, GlobalContextProvider, ElevatorButton, LoaderScreen, ModalCart, DrawerCategories }
 
 export class {{#pascalCase data.name}}{{/pascalCase}}OverrideModule extends Module {
     constructor(props: IModuleProps) {
