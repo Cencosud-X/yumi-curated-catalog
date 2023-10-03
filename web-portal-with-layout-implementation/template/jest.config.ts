fbf0ebd82926017ecc555d8b42c4eb6bae90bf69
CMA1551 <danielnelson.silvagutierrez@externos-cl.cencosud.com>
2023-10-03 16:47:06 -0300

    feat(web-portal-with-layout-implementation): :sparkles: add new template web yumi

---
+++ web-portal-with-layout-implementation/template/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'dummy',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/dummy',
+};
---
+++ web-portal-with-layout-implementation/template/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+export function App() {
+  return (
+    <>
+      <h1 className={styles.title} >Hello World</h1>
+      <div />
+    </>
+  );
+}
+
+export default App;
