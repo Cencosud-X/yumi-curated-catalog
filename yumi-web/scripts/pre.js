344ef510ac826c86b36103356f65541294c8f12e
CMA1551 <nelsondaniel.dev@gmail.com>
2023-10-11 17:59:13 -0300

    feat(yumi-web): Publishing new release

--- yumi-web/scripts/pre.js
+++ yumi-web/scripts/pre.js
@@ -1,20 +1,11 @@
 const path = require('path')
-
-const REQUIRED_VERSIONS = {
-  'react-dom': 16,
-  'react-router-dom': 5,
-  'react-router': 5,
-  nx: 14,
-  react: 16
-}
-
-const DEFAULT_VERSIONS = {
-  'react-dom': '^16.14.0',
-  'react-router-dom': '^5.3.4',
-  'react-router': '^5.2.0',
-  nx: '14.4.2',
-  react: '^16.14.0'
-}
+const {
+  requiredVersions: REQUIRED_VERSIONS,
+  defaultVersions: DEFAULT_VERSIONS,
+  scripts: SCRIPTS,
+  dependencies: DEPENDENCIES,
+  defaultErrorDependencyMessage: DEFAULT_ERROR_DEPENDENCY_MESSAGE
+} = require('../config.json')
 
 const getMajor = (version) => {
   const [major] = version
@@ -24,39 +15,32 @@ const getMajor = (version) => {
   return Number(major)
 }
 
-const SCRIPTS = {
-  'react-dom': 'npm install -S react-dom@',
-  'react-router-dom': 'npm install -S react-router-dom@',
-  'react-router': 'npm install -S react-router@',
-  nx: 'npm install -D @nrwl/web@',
-  react: 'npm install -S react@',
-}
-
 const verifyVersions = (rc) => {
-  const dependencies = ['react-router-dom', 'react-router', 'react-dom', 'react', 'nx']
   const packageJsonPath = path.join(rc.workspace_path, 'package.json')
   const packageDependencies = require(packageJsonPath).dependencies
   const packageDevDependencies = require(packageJsonPath).devDependencies
+  const dependencyErrors = []
 
-  const scripts = dependencies.map((dependency) => {
+  const dynamicScripts = DEPENDENCIES.map((dependency) => {
     const version = packageDependencies[dependency] || packageDevDependencies[dependency]
 
     if (version) {
       const major = getMajor(version)
 
       if (major !== REQUIRED_VERSIONS[dependency]) {
-        throw new Error(`Must have a ${dependency} version ${REQUIRED_VERSIONS[dependency]}.x.x`)
-      }
-
-      if (version !== DEFAULT_VERSIONS[dependency]) {
-        return `${SCRIPTS[dependency]}${version}`
+        dependencyErrors.push(
+          DEFAULT_ERROR_DEPENDENCY_MESSAGE
+            .replace('<dependency>', dependency)
+            .replace('<requiredVersion>', REQUIRED_VERSIONS[dependency])
+        )
+        return
       }
     } else {
       return `${SCRIPTS[dependency]}${DEFAULT_VERSIONS[dependency]}`
     }
   })
 
-  return scripts
+  return { dynamicScripts, dependencyErrors }
 }
 
 module.exports = async (runner, args) => {
@@ -69,7 +53,12 @@ module.exports = async (runner, args) => {
       `npx nx g @nrwl/web:app ${rc.path} --unitTestRunner=jest --e2eTestRunner=none`
     ]
 
-    const dynamicScripts = verifyVersions(rc).filter(Boolean)
+    const { dynamicScripts, dependencyErrors } = verifyVersions(rc).filter(Boolean)
+
+    if (dependencyErrors.length) {
+      throw new Error(dependencyErrors)
+    }
+
     const scripts = [...dynamicScripts, ...defaultScripts]
 
     await runner.execute(scripts, {
