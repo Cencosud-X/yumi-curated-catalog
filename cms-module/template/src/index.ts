fefec37d6829e63b83447a4be2384f4a5992e047
CMA1551 <nelsondaniel.dev@gmail.com>
2023-10-17 12:29:21 -0300

    feat(cms-module): Publishing new release

--- cms-module/template/src/index.ts
+++ cms-module/template/src/index.ts
@@ -1,11 +1,11 @@
 import * as Pages from './pages'
-// {{ #if data.settings.flow }}
-//  import * as Flow from './flow'
-// {{ /if }}
+{{ #if data.settings.flow }}
+ import * as Flow from './flow'
+{{ /if }}
 
 export {
   Pages,
-  // {{ #if }}
-  //  export Flow
-  // {{ /if }}
+  {{ #if }}
+   export Flow
+  {{ /if }}
 }
