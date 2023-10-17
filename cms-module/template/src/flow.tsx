e65abfbfe525b77548de2f363c580671f655fce7
CMA1551 <nelsondaniel.dev@gmail.com>
2023-10-17 14:25:39 -0300

    feat(cms-module): Publishing new release

--- cms-module/template/src/flow.tsx
+++ cms-module/template/src/flow.tsx
@@ -1,4 +1,4 @@
-//{{#if data.settings.flow}}
+{{#if data.settings.flow}}
 import React from 'react';
 import * as SDK from '@team_yumi/sdk';
 /* En la instalación de Seki, configurar si desea instalar el flujo o sólo las páginas. */
@@ -36,4 +36,4 @@ export default class Module extends SDK.Lib.BaseModule<IStaticParams> {
     this.Component = () => <Flow params={params} />;
   }
 }
-//{{/if}}
+{{/if}}
